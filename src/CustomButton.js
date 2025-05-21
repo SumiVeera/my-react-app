d3773174e6ffa9c661758dd7d7a4c1deeb9767aa
Sumiv <sumiv.career@gmail.com>
2025-05-21 16:27:48 -0500

    Added task2

---
+++ src/CustomButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+
+const ButtonComponent = ({ label, onClick, colorScheme = 'blue', size = 'md' }) => {
+    return (
+        <Button onClick={onClick} colorScheme={colorScheme} size={size}>
+            {label}
+        </Button>
+    );
+}
+export default ButtonComponent;
+    
